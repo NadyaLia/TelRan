03a400558c63904705697453f8454f85725dc0f9
NadyaLia <nadzeya.liashchynskaya@gmail.com>
2023-04-25 21:58:17 +0200

    Added homework sql queries

---
+++ HomeworkSQL/hr.sql
@@ -0,0 +1,5 @@
+SELECT * FROM hr.employees where department_id = 90;
+SELECT * FROM hr.employees where salary > 5000;
+SELECT * FROM hr.employees where last_name like 'L%';
+SELECT * FROM hr.departments where location_id = 1700;
+SELECT * FROM hr.locations where country_id = 'US';
