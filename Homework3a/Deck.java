216c0416857d9e4c88d0d33d4071ec57f155ccef
NadyaLia <nadzeya.liashchynskaya@gmail.com>
2023-05-08 20:03:33 +0200

    Revert "Added SeaBattle"
    This reverts commit 1ab9f046d73e56dd34770c6657fe97e92c7c9521.

--- Homework3a/Deck.java
+++
@@ -1,7 +0,0 @@
-package Homework.Homework3a;
-
-public class Deck {
-    public int x;
-    public int y;
-    public boolean hit;
-}
--- Homework3a/Field.java
+++
@@ -1,7 +0,0 @@
-package Homework.Homework3a;
-
-import java.util.ArrayList;
-
-public class Field {
-    public ArrayList<Ship> ships = new ArrayList<Ship>();
-}
