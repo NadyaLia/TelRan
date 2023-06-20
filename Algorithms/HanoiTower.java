e06c7e831dd551fee46dd0edd962cf7a03ffea7d
NadyaLia <nadzeya.liashchynskaya@gmail.com>
2023-06-20 22:53:34 +0200

    Added changes Hanoi Tower

--- Algorithms/HanoiTower.java
+++ Algorithms/HanoiTower.java
@@ -13,6 +13,47 @@ public class HanoiTower {
 
         recursiveHanoi(disksNumber, left, tmp, right);
     }
+// class HanoiMove {
+//    int n;
+//    char source;
+//    char target;
+//    char auxiliary;
+//
+//    HanoiMove(int n, char source, char target, char auxiliary){
+//        this.n=n;
+//        this.source = source;
+//        this.target = target;
+//        this.auxiliary = auxiliary;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception{
+//      int n = 3;
+//      char source = 'A';
+//      char target = 'C';
+//      char auxiliary = 'B';
+//
+//        Stack<HanoiMove> moves = new Stack<>();
+//        moves.push(new HanoiMove(n,source,target,auxiliary));
+//
+//        while (!moves.empty()){
+//            HanoiMove move = moves.pop();
+//
+//            if (move.n==1){
+//                System.out.println(move.source+""+move.target);
+//            }
+//            else {
+//                moves.push(new HanoiMove(move.n-1,move.auxiliary,move.target,move.source));
+//                moves.push(new HanoiMove(1,move.source,move.target,move.auxiliary));
+//                moves.push(new HanoiMove(move.n-1,move.source,move.auxiliary,move.target));
+//            }
+//        }
+//
+//    }
+//
+//}
+
 
     public static void iterativeHanoi(int disksNumber, char left, char tmp, char right) {
         int totalMoves = (int) Math.pow(2, disksNumber) - 1;
@@ -101,6 +142,9 @@ public class HanoiTower {
     public static void recursiveHanoi(int disksNumber, char left, char tmp, char right) {
         if (disksNumber == 0) {
             return;
+        }
+        if (disksNumber == 1) {
+            System.out.println("Move disk from " + left + " to " + right);
         }
         recursiveHanoi(disksNumber - 1, left, tmp, right);
         System.out.println("Move disk " + disksNumber + " from " + left + " to " + right);
